9747e0b1ca7f2efa3b740b09241de9183c11d08b
Filipe <123255512+F-i-l-i-p-e@users.noreply.github.com>
2023-09-08 20:35:30 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,7 +21,26 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// Fetch projects
+function showImage(imageSrc) {
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    overlay.innerHTML = `<img src="${imageSrc}" class="enlarged-image" alt="Enlarged Image">`;
+    document.body.appendChild(overlay);
+  
+    // Click to hide image
+    overlay.addEventListener('click', hideImage);
+    
+    // Esc to hide image
+    document.addEventListener('keydown', function(e) {
+        if (e.key === 'Escape') hideImage();
+    });
+}
+
+function hideImage() {
+    const overlay = document.querySelector('.overlay');
+    if (overlay) document.body.removeChild(overlay); // Remove overlay
+}
+
 function fetchProjects() {
     const url = 'https://raw.githubusercontent.com/F-i-l-i-p-e/PersonalPage/main/projects.json?' + new Date().getTime();
   
@@ -35,7 +54,7 @@ function fetchProjects() {
                 const projectCard = document.createElement('div');
                 projectCard.className = 'project-card d-flex flex-column';
                 projectCard.innerHTML = `
-                <img src="${project.image}" alt="${project.name}">
+                <img src="${project.image}" alt="${project.name}" class="project-image">
                 <h4>${project.name}</h4>
                 <p><strong>Description:</strong> ${project.description}</p>
                 <p><strong>Tech Stack:</strong> ${project.techStack}</p>
@@ -44,9 +63,27 @@ function fetchProjects() {
                 </div>
                 `;
                 container.appendChild(projectCard);
+
+                // Add click event to image if it's not the specified one
+                if (project.image !== "https://raw.githubusercontent.com/F-i-l-i-p-e/PersonalPage/main/images/gitLogo.png") {
+                    projectCard.querySelector('.project-image').addEventListener('click', function() {
+                        showImage(project.image);
+                    });
+                }
             });
         })
         .catch(error => {
             console.error('Error occurred:', error);
         });
 }
+
+// Scroll effect for the navbar
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 10) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
+});
